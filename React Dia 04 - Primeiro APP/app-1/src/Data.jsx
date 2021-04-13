08141e77eb7e79afa8a2f77c8adfd6f78d927455
Giovanni Marcon <gmarcontemp-git@yahoo.com>
2021-04-13 19:22:27 -0300

    Renomeados arquivos e add alterastate

--- React Dia 04 - Primeiro APP/app-1/src/Data.jsx
+++ React Dia 04 - Primeiro APP/app-1/src/Data.jsx
@@ -2,12 +2,12 @@ export const pessoa1 = {
     nome: "John Doe",
     idade: 29,
     formacao: "Indefinida",
-    status: "Pai ta on"
+    status: "Pai ta on",
 };
 
 export const pessoa2 = {
     nome: "Romero Brito",
     idade: 32,
     formacao: "Uma lá",
-    status: "Pai ta off"
+    status: "Pai ta off",
 };
---
+++ React Dia 04 - Primeiro APP/app-1/src/componentes/AlteraState.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+class AlteraState extends React.Component{
+    render(){
+        return(
+            ""
+        )
+    }
+}
+
+export default AlteraState;

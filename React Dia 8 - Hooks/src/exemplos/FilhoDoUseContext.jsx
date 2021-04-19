5f12940e600413776baf069ba46cefe0d73c1695
Giovanni Marcon <gmarcontemp-git@yahoo.com>
2021-04-19 00:01:42 -0300

    Atualizado Exercicios

--- React Dia 8 - Hooks/src/exemplos/FilhoDoUseContext.jsx
+++ React Dia 8 - Hooks/src/exemplos/FilhoDoUseContext.jsx
@@ -1,22 +1,14 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useContext, useLayoutEffect} from 'react';
 import {MeuContexto} from "./CreateContext"
 
 function FilhoDoUseContext() {
   const data = useContext(MeuContexto);
 
-  useEffect(
-    () => {
-      data.nome = "Trocou nome"
-      console.log("rodou")
-    }
-  )
-
   return(
     <>
       <p>Filho do useContext</p>
       <p>{data.cor}</p>
-      <p>{data.memoria}</p>
-      <p>Data trocada?  {data.nome}</p>
+      <p>{data.memoria} </p>
     </>
   )
 }

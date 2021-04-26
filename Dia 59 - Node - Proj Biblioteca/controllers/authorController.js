abc1d8cdb3e911183bcb949a0be98f583a63b7b7
Giovanni Marcon <gmarcontemp-git@yahoo.com>
2021-04-26 16:44:29 -0300

    Add Author List

--- Dia 59 - Node - Proj Biblioteca/controllers/authorController.js
+++ Dia 59 - Node - Proj Biblioteca/controllers/authorController.js
@@ -1,8 +1,16 @@
 var Author = require('../models/author');
+
 // Display list of all Authors.
-exports.author_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author list');
-};
+exports.author_list = function(req, res, next) {
+    Author.find()
+      .sort([['family_name', 'ascending']])
+      .exec(function (err, list_authors) {
+        if (err) { return next(err); }
+        //Successful, so render
+        res.render('author_list', { title: 'Author List', author_list: list_authors });
+      });
+  };
+
 // Display detail page for a specific Author.
 exports.author_detail = function(req, res) {
     res.send('NOT IMPLEMENTED: Author detail: ' + req.params.id);

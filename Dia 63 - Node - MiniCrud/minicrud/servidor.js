f044485061103e1487719058a1e0f418130e4f99
Giovanni Marcon <gmarcontemp-git@yahoo.com>
2021-04-28 18:07:28 -0300

    Adicionado Read

--- Dia 63 - Node - MiniCrud/minicrud/servidor.js
+++ Dia 63 - Node - MiniCrud/minicrud/servidor.js
@@ -9,6 +9,9 @@ const app = express();
 // Importamos o cliente de mongodb
 const MongoClient = require("mongodb").MongoClient;
 
+// Usando urlencoded com Middleware
+app.use(express.urlencoded({ extended: true }));
+
 // ▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲ //
 // -------- Conectando a database -------- //
 // ▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼ //
@@ -20,10 +23,10 @@ const URI =
 // Conectamos a o banco de dados em URI, e recebemos um callback que tem dois
 // parâmetros o primeiro é o erro, o segundo é a conexão com o banco de dados
 
-MongoClient.connect(URI, (err, client) => {
+MongoClient.connect(URI, { useUnifiedTopology: true }, (err, client) => {
   if (err) return console.log(err);
   db = client.db("minicrud2-aula");
-  console.log("BD Conectado")
+  console.log("BD Conectado");
 });
 
 // ▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲ //
@@ -50,4 +53,26 @@ app.listen(PORTA, () => {
 // enviamos nossa resposta;
 app.get("/", (req, res) => {
   res.render("index.ejs");
+});
+
+app.get("/mostrar", (re,res) => {
+  // Accesamos o banco de dados na coleçao "dados", fizemos uma busca "vazia"
+  // então achamos tudo, colocamos os resultados em uma Array
+  db.collection('dados').find().toArray((err, result) => {
+    if (err) return console.log(err);
+
+    res.render("mostrar.ejs", {dados: result})
+  })
+})
+
+// Mesma lógica mas com requisição POST em /mostrar
+app.post("/mostrar", (req, res) => {
+  // Salvamos nossa requisição na collection dados. .save aceita dois parâmetros
+  // a informação a ser salva e um callback com (erro, resultado);
+  db.collection("dados").save(req.body, (err, result) => {
+    if (err) return console.log(err);
+
+    console.log("Cadastrado no BD: " + req.body);
+    res.redirect("/");
+  });
 });

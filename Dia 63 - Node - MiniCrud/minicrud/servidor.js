99e358b1e9c99f87038e1a0fed78e70e503e66b7
Giovanni Marcon <gmarcontemp-git@yahoo.com>
2021-04-28 15:39:03 -0300

    Criado view

--- Dia 63 - Node - MiniCrud/minicrud/servidor.js
+++ Dia 63 - Node - MiniCrud/minicrud/servidor.js
@@ -3,14 +3,17 @@ const app = express()
 
 const PORTA = 3000
 
-// Inicia o servidor na porta
+// Inicia o servidor na porta PORTA
 app.listen(PORTA, () => {
-  console.log("Servido ativado em porta " + PORTA);
+  console.log("Servido ativado na porta " + PORTA);
 })
 
+// Setamos o EJS como nosso View
+app.set("view engine", "ejs")
+
 // Respondemos a requisiçoes GET no endereço "/" [Endpoint] com uma função
-// ["callback"] que tem dois parâmetros, o 1º recebe a requisição e mo segundo
+// ["callback"] que tem dois parâmetros, o 1º recebe a requisição e no segundo
 // enviamos nossa resposta;
 app.get('/', (req, res) => {
-  res.send("Olá, você está em '/'")
+  res.render("index.ejs")
 });

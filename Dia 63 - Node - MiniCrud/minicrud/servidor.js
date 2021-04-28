2aab2f985bb021ae3bd3ad0405bc405057c47b87
Giovanni Marcon <gmarcontemp-git@yahoo.com>
2021-04-28 16:00:51 -0300

    Adicionado link mdb

--- Dia 63 - Node - MiniCrud/minicrud/servidor.js
+++ Dia 63 - Node - MiniCrud/minicrud/servidor.js
@@ -1,6 +1,14 @@
+// Importa o express
 const express = require("express");
 const app = express()
 
+// Importamos o cliente de mongodb
+const MongoClient = require("mongodb").MongoClient
+
+// O endereço do BD
+const URI = "mongodb+srv://dbuser:dbuser@cluster0.msqwy.mongodb.net/minicrud2-aula?retryWrites=true&w=majority"
+
+// Porta que vamos usar
 const PORTA = 3000
 
 // Inicia o servidor na porta PORTA
@@ -8,7 +16,7 @@ app.listen(PORTA, () => {
   console.log("Servido ativado na porta " + PORTA);
 })
 
-// Setamos o EJS como nosso View
+// Setamos o EJS como nosso View para renderizar as páginas
 app.set("view engine", "ejs")
 
 // Respondemos a requisiçoes GET no endereço "/" [Endpoint] com uma função

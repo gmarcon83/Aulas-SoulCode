47aef3b3731c715b5d8de87a4a65dff957240594
Giovanni Marcon <gmarcontemp-git@yahoo.com>
2021-04-30 01:11:42 -0300

    Finalizado Crud Parcial

---
+++ Dia 64 - Node - Projeto Escola/routes/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+const disciplinas = require('../controllers/disciplinasController.js')
+const ObjectId = require("ObjectId")
+
+// Página Inicial.
+router.get('/', (req, res) => {
+  res.render("index.ejs");
+ });
+
+
+// Rotas Disciplinas
+
+// Listar
+router.get('/disciplinas', disciplinas.listar)
+
+// Cadastrar
+router.get('/disciplinas/nova', disciplinas.cadastrar)
+router.post('/disciplinas/nova', disciplinas.cadastrarPost)
+
+// Editar
+router.get('/disciplinas/editar/:id', disciplinas.editar)
+
+// Apagar
+router.get('/disciplinas/apagar/:id', disciplinas.apagar)
+
+
+
+
+
+
+
+
+
+module.exports = router;

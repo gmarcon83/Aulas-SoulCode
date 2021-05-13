aa07ad537c70a3cfb9a27199a35d8af141f80963
Giovanni Marcon <gmarcontemp-git@yahoo.com>
2021-05-13 20:38:14 -0300

    Criado Clientes

---
+++ ReactProjFinal/back/src/controllers/clienteController.js
@@ -0,0 +1,77 @@
+const Cliente = require("../models/clientes");
+const status = require("http-status");
+
+exports.Insert = (req, res, next) => {
+  Cliente.create({
+    ...req.body,
+  })
+    .then((cliente) => {
+      if (cliente) {
+        res.status(status.OK).send(cliente);
+      } else {
+        res.status(status.NOT_FOUND).send();
+      }
+    })
+    .catch((erro) => {
+      next(erro);
+    });
+};
+
+exports.SearchAll = (req, res, next) => {
+  Cliente.findAll()
+    .then((cliente) => {
+      res.status(status.OK).send(cliente);
+    })
+    .catch((erro) => next(erro));
+};
+
+exports.SearchOne = (req, res, next) => {
+  const id = req.params.id;
+
+  Cliente.findByPk(id)
+    .then((cliente) => {
+      if (cliente) {
+        res.status(status.OK).send(cliente);
+      }
+      res.status(status.NOT_FOUND).send();
+    })
+    .catch((erro) => next(erro));
+};
+
+exports.Delete = (req, res, next) => {
+  const id = req.params.id;
+
+  Cliente.findByPk(id)
+    .then((cliente) => {
+      if (cliente) {
+        cliente.destroy({
+          where: { id: id },
+        });
+        res.status(status.OK).send("Cliente deletado.");
+      }
+      res.status(status.NOT_FOUND).send();
+    })
+    .catch((erro) => next(erro));
+};
+
+exports.Update = (req, res, next) => {
+  const id = req.params.id;
+
+  Cliente.findByPk(id)
+    .then((cliente) => {
+      if (cliente) {
+        cliente
+          .update(
+            {
+              ...req.body,
+            },
+            {
+              where: { id: id },
+            }
+          )
+          .then(res.status(status.OK).send(cliente));
+      }
+      res.status(status.NOT_FOUND).send();
+    })
+    .catch((erro) => next(erro));
+};
--- ReactProjFinal/back/src/routes/routes.js
+++ ReactProjFinal/back/src/routes/routes.js
@@ -2,11 +2,20 @@ const express = require("express");
 const router = express.Router();
 
 const UsuariosController = require("../controllers/usuarioController");
+const ClientesController = require("../controllers/clienteController");
 
+// Rotas Usuarios
 router.post("/usuarios", UsuariosController.Insert);
 router.get("/usuarios", UsuariosController.SearchAll);
 router.get("/usuarios/:id", UsuariosController.SearchOne);
 router.put("/excluirUsuario/:id", UsuariosController.Delete);
 router.put("/editarUsuario/:id", UsuariosController.Update);
+
+// Rotas Clientes
+router.post("/clientes", ClientesController.Insert);
+router.get("/clientes", ClientesController.SearchAll);
+router.get("/clientes/:id", ClientesController.SearchOne);
+router.put("/excluirCliente/:id", ClientesController.Delete);
+router.put("/editarCliente/:id", ClientesController.Update);
 
 module.exports = router;

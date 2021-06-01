6955f7dffc8ff0f9cd9c32dbc1bc05847a86dd79
Giovanni Marcon <78059243+gmarcon83@users.noreply.github.com>
2021-06-01 14:14:46 -0300

    Iniciado Formulário

--- Angular Dia 01 - Intro/my-app/src/app/app.module.ts
+++ Angular Dia 01 - Intro/my-app/src/app/app.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { FirstComponent } from './first/first.component';
-import { SecondComponent } from './second/second.component';
 
 @NgModule({
-  declarations: [AppComponent, FirstComponent, SecondComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
--- Angular Dia 01 - Intro/my-app/src/app/first/first.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'first',
-  templateUrl: './first.component.html',
-  styleUrls: ['/first.component.css'],
-})
-export class FirstComponent {}

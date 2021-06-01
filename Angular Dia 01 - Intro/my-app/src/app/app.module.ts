4abbf0302eac8d0c2874bea0226f1a4ccd93dab5
Giovanni Marcon <78059243+gmarcon83@users.noreply.github.com>
2021-06-01 14:47:49 -0300

    Finalziado Form

--- Angular Dia 01 - Intro/my-app/src/app/app.module.ts
+++ Angular Dia 01 - Intro/my-app/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { StyledInputComponent } from './styled-input/styled-input.component';
+import { StyledButtonComponent } from './styled-button/styled-button.component';
 
 @NgModule({
-  declarations: [AppComponent, StyledInputComponent],
+  declarations: [AppComponent, StyledInputComponent, StyledButtonComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ Angular Dia 01 - Intro/my-app/src/app/styled-button/styled-button.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '[styled-button]', //seletor de atributo
+  templateUrl: './styled-button.component.html',
+  styleUrls: ['./styled-button.component.css'],
+})
+export class StyledButtonComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}

9f91fb84fc62e99e04838a5076f2bccc89a8a69b
Giovanni Marcon <78059243+gmarcon83@users.noreply.github.com>
2021-06-01 11:16:02 -0300

    Criado Interface ts

---
+++ Angular Dia 01 - Intro/user-project/index.ts
@@ -0,0 +1,97 @@
+interface UserProps {
+  firstName: string;
+  lastName: string;
+  userName: string;
+  password: string;
+  email: string;
+  cpf: string;
+  contact: string;
+  isAdmin: boolean;
+}
+
+class User implements UserProps {
+  firstName: string = "";
+  lastName: string = "";
+  userName: string = "";
+  password: string = "";
+  email: string = "";
+  cpf: string = "";
+  contact: string = "";
+  isAdmin: boolean = false;
+
+  constructor(
+    firstName: string,
+    lastName: string,
+    userName: string,
+    password: string,
+    email: string,
+    cpf: string,
+    contact: string,
+    isAdmin: boolean
+  ) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.userName = userName;
+    this.password = password;
+    this.email = email;
+    this.cpf = cpf;
+    this.contact = contact;
+    this.isAdmin = isAdmin;
+  }
+
+  getFirstName(): string {
+    return this.firstName;
+  }
+  setFirstName(firstName: string): void {
+    this.firstName = firstName;
+  }
+
+  getLastName(): string {
+    return this.lastName;
+  }
+  setLastName(lastName: string): void {
+    this.lastName = lastName;
+  }
+
+  getUserName(): string {
+    return this.userName;
+  }
+  setUserName(userName: string): void {
+    this.userName = userName;
+  }
+
+  getPassword(): string {
+    return this.password;
+  }
+  setPassword(password: string): void {
+    this.password = password;
+  }
+
+  getEmail(): string {
+    return this.email;
+  }
+  setEmail(email: string): void {
+    this.email = email;
+  }
+
+  getCpf(): string {
+    return this.cpf;
+  }
+  setCpf(cpf: string): void {
+    this.cpf = cpf;
+  }
+
+  getContact(): string {
+    return this.contact;
+  }
+  setContact(contact: string): void {
+    this.contact = contact;
+  }
+
+  getIsAdmin(): boolean {
+    return this.isAdmin;
+  }
+  setIsAdmin(isAdmin: boolean): void {
+    this.isAdmin = isAdmin;
+  }
+}

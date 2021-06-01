e3284323a2b5ba4b0613b2de51def777d1dd537e
Giovanni Marcon <78059243+gmarcon83@users.noreply.github.com>
2021-06-01 13:32:00 -0300

    criado primeiro componente

--- Angular Dia 01 - Intro/my-app/src/app/app.module.ts
+++ Angular Dia 01 - Intro/my-app/src/app/app.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, FirstComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ Angular Dia 01 - Intro/my-app/src/app/first/first.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'first',
+  templateUrl: './first.component.html',
+  styleUrls: ['/first.component.css'],
+})
+export class FirstComponent {}

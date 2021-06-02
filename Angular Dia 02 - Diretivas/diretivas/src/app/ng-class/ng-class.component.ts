5f458e7f563942437ae4e827b8b761a8da2c35c5
Giovanni Marcon <78059243+gmarcon83@users.noreply.github.com>
2021-06-02 10:15:43 -0300

    Finalizado Parte 2

--- Angular Dia 02 - Diretivas/diretivas/src/app/ng-class/ng-class.component.ts
+++ Angular Dia 02 - Diretivas/diretivas/src/app/ng-class/ng-class.component.ts
@@ -8,4 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class NgClassComponent {
   addBox: boolean = false;
   addBoxRed: boolean = false;
+
+  changeBox(event: any) {
+    this.addBox = event.target.checked;
+  }
+  changeBoxRed(event: any) {
+    this.addBoxRed = event.target.checked;
+  }
 }

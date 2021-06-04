0195ec4d5ae263d724e4366cb2969199d492c595
Giovanni Marcon <78059243+gmarcon83@users.noreply.github.com>
2021-06-04 01:41:00 -0300

    Criado Divisor

---
+++ Angular Dia 03 - Exercícios/SinglePage/src/app/divisor/divisor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-divisor',
+  templateUrl: './divisor.component.html',
+  styleUrls: ['./divisor.component.css']
+})
+export class DivisorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
